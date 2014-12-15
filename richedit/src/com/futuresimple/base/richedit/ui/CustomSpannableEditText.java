bdb66f8a0641860c096ca754bdaff9bc318b2303
Дмитро Дмитренко <dmytrenko.d@gmail.com>
2014-12-15 11:36:45 +0100

    Restore bullet list items properly.

--- richedit/src/com/futuresimple/base/richedit/ui/CustomSpannableEditText.java
+++ richedit/src/com/futuresimple/base/richedit/ui/CustomSpannableEditText.java
@@ -2,7 +2,9 @@ package com.futuresimple.base.richedit.ui;
 
 import com.futuresimple.base.richedit.text.EffectsHandler;
 import com.futuresimple.base.richedit.text.HtmlParsingListener;
+import com.futuresimple.base.richedit.text.style.BulletSpan;
 import com.futuresimple.base.richedit.text.style.URLSpan;
+import com.futuresimple.base.richedit.text.style.UnorderedListSpan;
 import com.nostra13.universalimageloader.core.ImageLoader;
 import com.nostra13.universalimageloader.core.assist.FailReason;
 import com.nostra13.universalimageloader.core.assist.ImageSize;
@@ -53,11 +55,23 @@ public class CustomSpannableEditText extends FixedSelectionEditText implements I
     super(context, attrs, defStyle);
   }
 
+  private void applyBulletLists(final List<BulletListHolder> bulletListHolders) {
+    for (final BulletListHolder bulletListHolder : bulletListHolders) {
+      final UnorderedListSpan bulletList = new UnorderedListSpan();
+      getText().setSpan(bulletList, bulletListHolder.getStart(), bulletListHolder.getEnd(), Spanned.SPAN_PARAGRAPH);
+      for (final BulletHolder bulletHolder : bulletListHolder.getBulletHolders()) {
+        final BulletSpan bullet = new BulletSpan(bulletHolder.getRadius(), bulletHolder.getGap(), bulletHolder.getColor(), bulletHolder.isWantColor());
+        getText().setSpan(bullet, bulletHolder.getStart(), bulletHolder.getEnd(), Spanned.SPAN_PARAGRAPH);
+        bulletList.addItem(bullet);
+      }
+    }
+  }
+
   private void applyLinks(final List<LinkHolder> linkHolders) {
     for (final LinkHolder linkHolder : linkHolders) {
       getText().setSpan(
           new URLSpan(linkHolder.getLink(), linkHolder.getTitle(), linkHolder.getTarget()),
-          linkHolder.getLinkStart(), linkHolder.getLinkEnd(),
+          linkHolder.getStart(), linkHolder.getEnd(),
           Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
       );
     }
@@ -75,10 +89,34 @@ public class CustomSpannableEditText extends FixedSelectionEditText implements I
       super.onRestoreInstanceState(customState.getSuperState());
     }
 
+    applyBulletLists(customState.getBulletListHolders());
     applyLinks(customState.getLinkHolders());
 
     mLastState = null;
     mStateRestored = true;
+  }
+
+  private List<BulletListHolder> removeAllBulletLists() {
+    final List<BulletListHolder> bulletListHolders = new ArrayList<>();
+    final UnorderedListSpan[] bulletLists = getText().getSpans(0, getText().length(), UnorderedListSpan.class);
+    for (final UnorderedListSpan bulletList: bulletLists) {
+      final BulletListHolder bulletListHolder = new BulletListHolder(getText().getSpanStart(bulletList), getText().getSpanEnd(bulletList));
+      for (final BulletSpan bulletSpan : bulletList.getItems()) {
+        bulletListHolder.addBulletHolder(new BulletHolder(
+            bulletSpan.getBulletRadius(),
+            bulletSpan.getGapWidth(),
+            bulletSpan.getColor(),
+            bulletSpan.isWantColor(),
+            getText().getSpanStart(bulletSpan),
+            getText().getSpanEnd(bulletSpan)
+        ));
+        getText().removeSpan(bulletSpan);
+      }
+      bulletListHolders.add(bulletListHolder);
+      getText().removeSpan(bulletList);
+    }
+
+    return bulletListHolders;
   }
 
   private List<LinkHolder> removeAllLinks() {
@@ -96,10 +134,11 @@ public class CustomSpannableEditText extends FixedSelectionEditText implements I
   public Parcelable onSaveInstanceState() {
     // do not call removeAllLinks() inline!!!
     // All "removing" operations have to be done before calling super.onSaveInstanceState() !
+    final List<BulletListHolder> bulletListHolders = removeAllBulletLists();
     final List<LinkHolder> linkHolders = removeAllLinks();
 
     mStateRestored = false;
-    mLastState = new CustomSpansState(super.onSaveInstanceState(), linkHolders);
+    mLastState = new CustomSpansState(super.onSaveInstanceState(), bulletListHolders, linkHolders);
     return mLastState;
   }
 
@@ -152,12 +191,53 @@ public class CustomSpannableEditText extends FixedSelectionEditText implements I
 
   static final class CustomSpansState extends BaseSavedState {
 
+    private final List<BulletListHolder> mBulletListHolders = new ArrayList<>();
     private final List<LinkHolder> mLinkHolders = new ArrayList<>();
 
     public CustomSpansState(final Parcel source) {
       super(source);
 
+      readBulletLists(source);
       readLinks(source);
+    }
+
+    public CustomSpansState(final Parcelable superState, final List<BulletListHolder> bulletListHolders, final List<LinkHolder> linkHolders) {
+      super(superState);
+
+      if (bulletListHolders != null) {
+        mBulletListHolders.addAll(bulletListHolders);
+      }
+
+      if (linkHolders != null) {
+        mLinkHolders.addAll(linkHolders);
+      }
+    }
+
+    private void readBulletLists(final Parcel source) {
+      final int bulletListsCount = source.readInt();
+      if (bulletListsCount > 0) {
+        for (int i = 0; i < bulletListsCount; i++) {
+
+          final BulletListHolder bulletListHolder = new BulletListHolder(source.readInt(), source.readInt());
+
+          final int bulletsCount = source.readInt();
+
+          if (bulletsCount > 0) {
+            for (int j = 0; j < bulletsCount; j++) {
+              bulletListHolder.addBulletHolder(new BulletHolder(
+                  source.readInt(),
+                  source.readInt(),
+                  source.readInt(),
+                  source.readInt() != 0,
+                  source.readInt(),
+                  source.readInt()
+              ));
+            }
+          }
+
+          mBulletListHolders.add(bulletListHolder);
+        }
+      }
     }
 
     private void readLinks(final Parcel source) {
@@ -175,16 +255,29 @@ public class CustomSpannableEditText extends FixedSelectionEditText implements I
       }
     }
 
-    public CustomSpansState(final Parcelable superState, final List<LinkHolder> linkHolders) {
-      super(superState);
-
-      if (linkHolders != null) {
-        mLinkHolders.addAll(linkHolders);
-      }
+    public final List<BulletListHolder> getBulletListHolders() {
+      return mBulletListHolders;
     }
 
     public final List<LinkHolder> getLinkHolders() {
       return mLinkHolders;
+    }
+
+    private void writeBulletLists(final Parcel destination) {
+      destination.writeInt(mBulletListHolders.size());
+      for (final BulletListHolder bulletListHolder : mBulletListHolders) {
+        destination.writeInt(bulletListHolder.getStart());
+        destination.writeInt(bulletListHolder.getEnd());
+        destination.writeInt(bulletListHolder.getBulletHolders().size());
+        for (final BulletHolder bulletHolder : bulletListHolder.getBulletHolders()) {
+          destination.writeInt(bulletHolder.getRadius());
+          destination.writeInt(bulletHolder.getGap());
+          destination.writeInt(bulletHolder.getColor());
+          destination.writeInt(bulletHolder.isWantColor() ? 1 : 0);
+          destination.writeInt(bulletHolder.getStart());
+          destination.writeInt(bulletHolder.getEnd());
+        }
+      }
     }
 
     private void writeLinks(final Parcel destination) {
@@ -193,8 +286,8 @@ public class CustomSpannableEditText extends FixedSelectionEditText implements I
         destination.writeString(linkHolder.getLink());
         destination.writeString(linkHolder.getTitle());
         destination.writeString(linkHolder.getTarget());
-        destination.writeInt(linkHolder.getLinkStart());
-        destination.writeInt(linkHolder.getLinkEnd());
+        destination.writeInt(linkHolder.getStart());
+        destination.writeInt(linkHolder.getEnd());
       }
     }
 
@@ -202,6 +295,7 @@ public class CustomSpannableEditText extends FixedSelectionEditText implements I
     public void writeToParcel(final Parcel destination, final int flags) {
       super.writeToParcel(destination, flags);
 
+      writeBulletLists(destination);
       writeLinks(destination);
     }
 
@@ -220,19 +314,38 @@ public class CustomSpannableEditText extends FixedSelectionEditText implements I
         };
   }
 
-  static final class LinkHolder {
+  static class SpanHolder {
+    private final int mStart;
+    private final int mEnd;
+
+    public SpanHolder(final int start, final int end) {
+      mStart = start;
+      mEnd = end;
+    }
+
+    public final int getStart() {
+      return mStart;
+    }
+
+    public final int getEnd() {
+      return mEnd;
+    }
+
+    public final int getSize() {
+      return mEnd - mStart;
+    }
+  }
+
+  static final class LinkHolder extends SpanHolder {
     private final String mLink;
     private final String mTitle;
     private final String mTarget;
-    private final int mLinkStart;
-    private final int mLinkEnd;
 
-    public LinkHolder(final String link, final String title, final String target, final int linkStart, final int linkEnd) {
+    public LinkHolder(final String link, final String title, final String target, final int start, final int end) {
+      super(start, end);
       mLink = link;
       mTitle = title;
       mTarget = target;
-      mLinkStart = linkStart;
-      mLinkEnd = linkEnd;
     }
 
     public final String getLink() {
@@ -246,13 +359,55 @@ public class CustomSpannableEditText extends FixedSelectionEditText implements I
     public final String getTarget() {
       return mTarget;
     }
+  }
 
-    public final int getLinkStart() {
-      return mLinkStart;
+  static final class BulletHolder extends SpanHolder {
+
+    private final int mRadius;
+    private final int mColor;
+    private final int mGap;
+    private final boolean mWantColor;
+
+    public BulletHolder(final int radius, final int gap, final int color, final boolean wantColor, final int start, final int end) {
+      super(start, end);
+      mRadius = radius;
+      mColor = color;
+      mGap = gap;
+      mWantColor = wantColor;
     }
 
-    public final int getLinkEnd() {
-      return mLinkEnd;
+    public final int getRadius() {
+      return mRadius;
+    }
+
+    public final int getColor() {
+      return mColor;
+    }
+
+    public final int getGap() {
+      return mGap;
+    }
+
+    public final boolean isWantColor() {
+      return mWantColor;
+    }
+  }
+
+  static final class BulletListHolder extends SpanHolder {
+    private List<BulletHolder> mBulletHolders = new ArrayList<>();
+
+    public BulletListHolder(int start, int end) {
+      super(start, end);
+    }
+
+    public final void addBulletHolder(final BulletHolder bulletHolder) {
+      if (bulletHolder != null) {
+        mBulletHolders.add(bulletHolder);
+      }
+    }
+
+    public final List<BulletHolder> getBulletHolders() {
+      return mBulletHolders;
     }
   }
 }
